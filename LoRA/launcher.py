f8dd40e473eb30800b19fd86ea1b9232a8583ee9
xushilianzhuren <852456426@qq.com>
2026-09-14 17:49:52 +0800

    launcher v3 fix

--- LoRA/launcher.py
+++ LoRA/launcher.py
@@ -1,9 +1,9 @@
-# 分离启动器v2·自带下载训练脚本·2026-09-14
-import subprocess, urllib.request
-url="https://gh-proxy.com/https://raw.githubusercontent.com/xushilianzhuren/zhishen-empire/main/LoRA/train_stage1.py?t="+str(__import__("time").time())"
-urllib.request.urlretrieve(url, "/home/aistudio/train_stage1.py?t="+str(__import__("time").time())")
-print("SCRIPT_DOWNLOADED")
-subprocess.Popen(["python","/home/aistudio/train_stage1.py?t="+str(__import__("time").time())"],
+# 分离启动器v3·破缓存下载·2026-09-14
+import subprocess, urllib.request, time
+url = "https://gh-proxy.com/https://raw.githubusercontent.com/xushilianzhuren/zhishen-empire/main/LoRA/train_stage1.py?t=" + str(int(time.time()))
+urllib.request.urlretrieve(url, "/home/aistudio/train_stage1.py")
+print("SCRIPT_DOWNLOADED", len(open("/home/aistudio/train_stage1.py").read()))
+subprocess.Popen(["python","/home/aistudio/train_stage1.py"],
     stdout=open("/home/aistudio/train_stage1.log","w"),
     stderr=subprocess.STDOUT, start_new_session=True)
 print("TRAIN_LAUNCHED")
